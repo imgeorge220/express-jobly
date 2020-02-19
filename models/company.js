1884ef38e9c4b8d9751ca2ecb2fc99b07d421fc7
George Gruner <imgeorge220@gmail.com>
2020-02-19 12:02:21 -0800

    update, delete, add Company methods

--- models/company.js
+++ models/company.js
@@ -57,7 +57,7 @@ class Company {
         throw new ExpressError("Max value cannot be lower than min value", 400);
       }
       let filterParams = buildFilter(queries);
-      
+
       const results = await db.query(
         `SELECT 
           handle, 
@@ -79,7 +79,74 @@ class Company {
     }
   }
 
+  static async update(handle, items) {
+    try {
+      let queryParams = sqlForPartialUpdate("companies", items, "handle", handle);
+
+      const update = await db.query(
+        queryParams.query,
+        [queryParams.values]
+      );
+
+      if (update.rows.length === 0) {
+        throw new ExpressError('Company does not exist', 404);
+      }
+
+      let company = new Company(update.rows[0]);
+
+      return { company };
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  async addToDb() {
+    try {
+      const company = await db.query(
+        `INSERT INTO companies
+          (handle,
+          name,
+          description,
+          num_employees AS numEmployees,
+          logo_url AS logoUrl)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING
+          handle,
+          name,
+          description,
+          num_employees AS numEmployees,
+          logo_url AS logoUrl`,
+        [this.handle,
+        this.name,
+        this.description,
+        this.numEmployees,
+        this.logoUrl]
+      );
+
+      return { company };
+    } catch (err) {
+      return next(err);
+    }
+  }
 
 
+  static async delete(handle) {
+    try {
+      const deleted = await db.query(
+        `DELETE FROM companies
+        WHERE handle = $1`,
+        [handle]
+      );
+
+      console.log(deleted);
+      if (deleted.rows.length === 0) {
+        throw new ExpressError('Company does not exist', 404);
+      }
+
+      return { message: "Company successfully deleted"};
+    } catch (err) {
+      return next(err);
+    }
+  }
 
 }
