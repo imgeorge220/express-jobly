c5a7916e176f28d4e1fc71e4d7c95207805465b8
George Gruner <imgeorge220@gmail.com>
2020-02-19 10:15:36 -0800

    create data.sql

--- data.sql
+++ data.sql
@@ -1,20 +1,9 @@
--- \c jobly
+DROP TABLE IF EXISTS companies;
 
--- DROP TABLE IF EXISTS companies;
-
--- CREATE TABLE companies (
---     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
---     first_name text NOT NULL,
---     last_name text NOT NULL,
---     phone text,
---     notes text DEFAULT '' NOT NULL
--- );
-
--- CREATE TABLE reservations (
---     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
---     customer_id integer NOT NULL REFERENCES customers,
---     start_at timestamp without time zone NOT NULL,
---     num_guests integer NOT NULL,
---     notes text DEFAULT '' NOT NULL,
---     CONSTRAINT reservations_num_guests_check CHECK ((num_guests > 0))
--- );
+CREATE TABLE companies (
+    handle text PRIMARY KEY,
+    name text NOT NULL UNIQUE, 
+    num_employees integer,
+    description text,
+    logo_url text
+);
